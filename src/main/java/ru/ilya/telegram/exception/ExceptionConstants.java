71e9fde79e48c513be63eb6d5ac863fb63d0be53
devourmealive <foxmanlisichka@yandex.ru>
2023-11-16 02:15:01 +0300

    TG-2 removed useless for now exceptions

--- src/main/java/ru/ilya/telegram/exception/ExceptionConstants.java
+++
@@ -1,9 +0,0 @@
-package ru.ilya.telegram.exception;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class ExceptionConstants {
-    public final String SERVICE_UNAVAILABLE = "Endpoint %s unavailable";
-
-}
